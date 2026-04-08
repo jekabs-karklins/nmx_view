b9afe7a831426c49838c6a0f4b660e05f30ff7de
jekabskarklins <jekabs.karklins@ess.eu>
2026-04-08 10:20:42 +0200

    fix: make base url configurable

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-plugins: [react()],
+  base: process.env.VITE_BASE ?? '/',
+  plugins: [react()],
   optimizeDeps: {
     exclude: ['h5wasm'],
   },
